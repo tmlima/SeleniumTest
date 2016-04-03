bcad83fa442552127db93f772708a54f5b44bfc4
tmlima <thiago.m.lima@outlook.com>
2016-04-03 19:14:39 -0300

    Implementing test Problema2

--- SeleniumTest/DesktopTests.cs
+++ SeleniumTest/DesktopTests.cs
@@ -1,16 +1,52 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 
 namespace SeleniumTest
 {
     [TestClass]
     public class DesktopTests
     {
-        //[TestMethod]
-        //public void Problema2()
-        //{
-        //}
+        const string FakeContentFileName = "fake.txt";
+        const string FakeContent = "Fake content.";
+
+        [TestInitialize]
+        public void MyTestInitialize()
+        {
+            CreateTxtFileWithContent( FakeContentFileName, FakeContent );
+        }
+
+        /// <summary>
+        /// Pré requisitos: Criar um arquivo.txt com conteúdo fake.
+        /// Condições: A biblioteca do Word não poderá ser utilizada.
+        /// Abrir o Microsoft Word;
+        /// Carregar o conteudo do arquivo texto previamente criado para dentro do Word;
+        /// Bater um printscreen da tela do Word;
+        /// Salvar o arquivo Word com TimeStamp, no mesmo folder do arquivo.txt;
+        /// Deletar o arquivo.txt e manter o arquivo Word.
+        /// </summary>
+        [TestMethod]
+        public void Problema2()
+        {
+            File.Delete( FakeContentFileName );
+        }
+
+        private void CreateTxtFileWithContent(string fileName, string content)
+        {
+            StreamWriter stream = null;
+            try
+            {
+                stream = File.CreateText( fileName );
+                stream.Write( content );
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                if ( stream != null )
+                    stream.Close();
+            }
+        }
     }
 }
